24b9b6d1bfc986a431984d275a96e44e7ca7344e
Alex Kim <alex.kim9905@gmail.com>
2025-10-20 11:30:12 +1300

    define third operator

--- Weather Data ETL Pipeline (Airflow, AWS S3)/airflow/dags/weather_dag.py
+++ Weather Data ETL Pipeline (Airflow, AWS S3)/airflow/dags/weather_dag.py
@@ -2,7 +2,39 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import HttpOperator
+from airflow.providers.standard.operators.python import PythonOperator
+import pandas as pd
 import json
+
+def transform_load_data(task_instance, **kwargs):
+    # pull data from previous task
+    data = task_instance.xcom_pull(task_ids='extract_weather_data')
+    
+    # Transform: create a simplified dictionary with relevant fields
+    transformed_data = {
+        'City': data['name'],
+        'Description': data['weather'][0]['description'],
+        'Temperature (C)': round(data['main']['temp'] - 273.15, 1),
+        'Feels Like (C)': round(data['main']['feels_like'] - 273.15, 1),
+        'Minimum Temperature (C)': round(data['main']['temp_min'] - 273.15, 1),
+        'Maximum Temperature (C)': round(data['main']['temp_max'] - 273.15, 1),
+        'Pressure': data['main']['pressure'],
+        'Humidity': data['main']['humidity'],  
+        'Wind Speed': data['wind']['speed'],
+        'Time of Record': datetime.fromtimestamp(data['dt'] + data['timezone']),
+        'Sunrise (local time)': datetime.fromtimestamp(data['sys']['sunrise'] + data['timezone']),
+        'Sunset (local time)': datetime.fromtimestamp(data['sys']['sunset'] + data['timezone'])
+    }
+
+    # convert dictionary to DataFrame
+    df_data = pd.DataFrame([transformed_data])
+
+    # save the transformed data to a CSV file with timestamp
+    now = datetime.now().strftime("%d%m%Y_%H%M%S")
+    filename = f"current_weather_data_auckland_{now}.csv"
+    df_data.to_csv(filename, index=False)
+
+
 
 default_args = {
     'owner': 'airflow',
@@ -37,5 +69,11 @@ with DAG(
         log_response=True
     )
 
+    # task to transform and load the weather data
+    transform_load_weather_data = PythonOperator(
+        task_id='transform_load_weather_data',
+        python_callable=transform_load_data # function defined above
+    )
+
     # set task dependencies
-    is_weather_api_responsive >> extract_weather_data
+    is_weather_api_responsive >> extract_weather_data >> transform_load_weather_data
