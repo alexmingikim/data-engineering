6cbbf6f488283d3c0e0226b1cad2b69763e7524d
Alex Kim <alex.kim9905@gmail.com>
2025-10-29 15:17:20 +1300

    establish dependency between raw data and staging layer

--- Data Analysis with dbt/netflix/models/staging/src_ratings.sql
+++ Data Analysis with dbt/netflix/models/staging/src_ratings.sql
@@ -1,10 +1,8 @@
 -- testing incremental model
-{{ config(
-    materialized = 'table'
-) }}
+{{ config( materialized = 'table') }}
 
 WITH raw_ratings AS (
-  SELECT * FROM MOVIELENS.RAW.RAW_RATINGS
+  SELECT * FROM {{ source('netflix', 'r_ratings') }}
 )
 
 SELECT
